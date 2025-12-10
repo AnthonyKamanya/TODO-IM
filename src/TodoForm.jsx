e0d1bf1ccd7b352e0a863717094e24d44a0c2ad4
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-12-10 09:09:55 +0000

    feat: Re-focus Input After Submit

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,16 +1,19 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 const TodoForm = ({ onAddTodo }) => {
+  const todoTitleInput = useRef('');
   const handleAddTodo = (event) => {
     event.preventDefault();
     const title = event.target.title;
     onAddTodo(title);
     event.target.title.value = '';
+    todoTitleInput.current.focus();
   };
+
   return (
     <>
       <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Todo</label>
-        <input id="todoTitle" name="title"></input>
+        <input ref={todoTitleInput} id="todoTitle" name="title"></input>
         <button>Add Todo</button>
       </form>
     </>
