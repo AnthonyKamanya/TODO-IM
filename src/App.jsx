d9f5e80a214475df2384a12eb5815f7d6f04399a
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2025-12-10 08:58:44 +0000

    feat: Preparing App to manage Todo state and add an Event Handler to the Form

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import TodoForm from './TodoForm';
 import { useState } from 'react';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('Day One');
+  const [todoList, setTodoList] = useState([]);
+  const addTodo = (title) => {
+    const newTodo = { title: title, id: Date.now() };
+    setTodoList([...todoList, newTodo]);
+  };
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
+      <TodoForm onAddTodo={addTodo} />
+      {/* <p>{todoList}</p> */}
       <TodoList />
     </div>
   );
--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,9 +1,16 @@
-const TodoForm = () => {
+import { useRef } from "react";
+const TodoForm = ({ onAddTodo }) => {
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const title = event.target.title;
+    onAddTodo(title);
+    event.target.title.value = '';
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <label htmlFor="todoTitle">Todo</label>
-        <input id="todoTitle"></input>
+        <input id="todoTitle" name="title"></input>
         <button>Add Todo</button>
       </form>
     </>
