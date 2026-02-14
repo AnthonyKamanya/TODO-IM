54f91022e9f01e2edd8f6a59877de1ef2f3727af
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2026-02-14 00:44:53 +0000

    Duplicate State Logic to Reducer

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -26,6 +26,7 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
+    //useEffect (Pessimistic UI)
     case actions.fetchTodos:
       return {
         ...state,
@@ -53,6 +54,78 @@ function reducer(state = initialState, action) {
         ...state,
         errorMessage: action.error.message,
         isLoading: false,
+      };
+
+    //addTodo(Pessimistic UI)
+    case actions.startRequest:
+      return { ...state, isSaving: true };
+    case actions.addTodo:
+      const record = action.records[0];
+      const savedTodo = {
+        id: record.id,
+        title: record.fields.title,
+        isCompleted: record.fields.isCompleted ?? false,
+      };
+      if (!record.fields.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+      return {
+        ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
+      };
+    case actions.endRequest:
+      return { ...state, isLoading: false, isSaving: false };
+    case actions.setLoadError:
+      return { ...state, errorMessage: action.error.message, isLoading: false };
+
+    //updateTodo,completeTodo(Optimistic UI)
+    case actions.updateTodo:
+      //Save the ORIGINAL todo (your undo button)
+      const originalTodo = action.todoList.find(
+        (todo) => todo.id === editedTodo.id
+      );
+      // Optimistically update the UI (instant feedback)
+      const editedUpdatedTodos = state.todoList.map((todo) => {
+        if (todo.id === action.editedTodo.id) {
+          return { ...action.editedTodo };
+        } else {
+          return todo;
+        }
+      });
+
+      return {
+        ...state,
+        todoList: [editedUpdatedTodos],
+        errorMessage: action.error.message,
+      };
+    case actions.completeTodo:
+      const completedUpdatedTodos = state.todoList.map((todo) =>
+        todo.id === action.id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo
+      );
+
+      return {
+        ...state,
+        todoList: completedUpdatedTodos,
+      };
+
+    case actions.revertTodo:
+      const revertedTodos = action.todoList.map((todo) =>
+        todo.id === originalTodo.id ? originalTodo : todo
+      );
+      return {
+        ...state,
+        errorMessage: action.error.message,
+        todoList: [revertedTodos],
+      };
+
+    //Dismiss Error Button
+    case actions.clearError:
+      return {
+        ...state,
+        errorMessage: '',
       };
     default:
       return state;
