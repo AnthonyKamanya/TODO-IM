0ba967f730a9e77cb9edaaebf179461c29b1c0d9
Anthony-Kamanya <Anthony.Kamanya@autotrader.co.uk>
2026-02-18 23:44:34 +0000

    styling Header

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -4,25 +4,28 @@ import styles from './Header.module.css';
 const Header = ({ title }) => {
   return (
     <>
-      <h1 className={styles.title}> {title}</h1>
-      <nav>
-        <NavLink
-          to={'/'}
-          className={({ isActive }) => {
-            return isActive ? styles.active : styles.inactive;
-          }}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to={'/about'}
-          className={({ isActive }) => {
-            return isActive ? styles.active : styles.inactive;
-          }}
-        >
-          About
-        </NavLink>
-      </nav>
+      <div className={styles.header}>
+        {' '}
+        <h1 className={styles.title}> {title}</h1>
+        <nav>
+          <NavLink
+            to={'/'}
+            className={({ isActive }) => {
+              return isActive ? styles.active : styles.inactive;
+            }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to={'/about'}
+            className={({ isActive }) => {
+              return isActive ? styles.active : styles.inactive;
+            }}
+          >
+            About
+          </NavLink>
+        </nav>
+      </div>
     </>
   );
 };
